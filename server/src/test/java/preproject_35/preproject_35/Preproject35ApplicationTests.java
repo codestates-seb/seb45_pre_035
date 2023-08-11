88a5dfd010b0c518e6edbe8106d619276a862936
tjddnjs0102 <rjf654@gmail.com>
2023-08-11 14:50:22 +0900

    please

---
+++ server/src/test/java/preproject_35/preproject_35/Preproject35ApplicationTests.java
@@ -0,0 +1,13 @@
+package preproject_35.preproject_35;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Preproject35ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
