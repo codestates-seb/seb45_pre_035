e98f3809248c3aceff4dbdf1cef24b1fde6e4210
Moses-Yu <mosesyumss@gmail.com>
2023-08-14 11:50:52 +0900

    eslint 설정 수정, npm install eslint-plugin-storybook

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Home from './Pages/Home'
-import MyPage from './Pages/MyPage'
-import SignIn from './Pages/SignIn'
-import SignUp from './Pages/SignUp'
-import NewQuestion from './Pages/NewQuestion'
-import QuestionDetail from './Pages/QuestionDetail'
+import Home from './Pages/Home';
+import MyPage from './Pages/MyPage';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+import NewQuestion from './Pages/NewQuestion';
+import QuestionDetail from './Pages/QuestionDetail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route exact path ="/" element={<Home />} />
-          <Route path="/mypage" element={<MyPage />} />
-         <Route path="/signin" element={<SignIn />} />
-         <Route path="/signup" element={<SignUp />} />
-         <Route path="/newquestion" element={<NewQuestion />} />
-         <Route path="/questiondetail/:id" element={<QuestionDetail />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/newquestion" element={<NewQuestion />} />
+        <Route path="/questiondetail/:id" element={<QuestionDetail />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default App;
