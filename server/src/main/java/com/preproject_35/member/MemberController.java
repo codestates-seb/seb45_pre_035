099e0f919ab6c194bb3e4a8f5eefc9fe13ef3e0b
fsnzsaber <fsnzsaber@naver.com>
2023-08-14 13:27:18 +0900

    패키지 클래스 생성

--- server/src/main/java/com/preproject_35/member/MemberController.java
+++ server/src/main/java/com/preproject_35/member/MemberController.java
@@ -1,4 +1,20 @@
 package com.preproject_35.member;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/v1/members")
 public class MemberController {
+    @PostMapping
+    public ResponseEntity postMember(@Valid MemberDTO memberDto) {
+        return new ResponseEntity<>(memberDto, HttpStatus.CREATED);
+    }
+
 }
--- server/src/main/java/com/preproject_35/member/MemberService.java
+++ server/src/main/java/com/preproject_35/member/MemberService.java
@@ -1,4 +1,10 @@
 package com.preproject_35.member;
 
+import javax.servlet.http.HttpSession;
+
 public class MemberService {
+    public String loginCheck(MemberDTO dto, HttpSession session) {
+        return null;
+    }
+
 }
