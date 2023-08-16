b7f7e3812bb7628ca945ca55ed5e90ed794c0bde
Moses-Yu <mosesyumss@gmail.com>
2023-08-16 11:07:18 +0900

    글로벌 스타일 파일 추가, 스토리북에 글로벌 스타일 적용

---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  /* Your global CSS styles go here */
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyles;
