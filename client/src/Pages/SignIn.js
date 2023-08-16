ea7bfd7ea4923d67e3e14208bcde32506c93b52f
Moses-Yu <mosesyumss@gmail.com>
2023-08-16 14:07:20 +0900

    .

--- client/src/Pages/SignIn.js
+++ client/src/Pages/SignIn.js
@@ -2,7 +2,12 @@ import React from 'react';
 import { Button } from '../Components/Button';
 import { SignInBox } from './styles/SignInBox';
 import { PageStyle } from './styles/PageStyle';
+import { useState } from 'react';
 export default function SignIn() {
+  const [id, setId] = useState('');
+  const onChangeHandler = (e) => {
+    setId(e.target.value);
+  };
   return (
     <PageStyle>
       <SignInBox>
@@ -10,7 +15,7 @@ export default function SignIn() {
           <div className="title">로그인</div>
           <div className="input-container">
             <img src="/images/ic-outline-email.png" alt=""></img>
-            <input type="text"></input>
+            <input type="text" onChange={onChangeHandler} value={id}></input>
           </div>
           <div className="input-container">
             <img src="/images/mdi-password-outline.png" alt=""></img>
