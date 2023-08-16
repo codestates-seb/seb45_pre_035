c06725dd3b2c3a38468e96a35d27860f98a92e34
fsnzsaber <fsnzsaber@naver.com>
2023-08-16 09:11:56 +0900

    패키지 클래스 생성

--- server/src/main/java/com/preproject_35/member/MemberController.java
+++ server/src/main/java/com/preproject_35/member/MemberController.java
@@ -3,7 +3,6 @@ package com.preproject_35.member;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
