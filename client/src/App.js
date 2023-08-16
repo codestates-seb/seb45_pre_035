5b3ee21c9e8a265c72056d490942636da4169b34
Moses-Yu <mosesyumss@gmail.com>
2023-08-16 11:07:26 +0900

    글로벌 스타일 파일 추가, 스토리북에 글로벌 스타일 적용

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,22 @@ import SignUp from './Pages/SignUp';
 import NewQuestion from './Pages/NewQuestion';
 import QuestionDetail from './Pages/QuestionDetail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyles from './GlobalStyles';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/newquestion" element={<NewQuestion />} />
-        <Route path="/questiondetail/:id" element={<QuestionDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/newquestion" element={<NewQuestion />} />
+          <Route path="/questiondetail/:id" element={<QuestionDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
