a6cf67116931c18bb09bce5eb737a47510600a08
seolmiseon <budaxige@gmail.com>
2023-08-16 15:11:16 +0900

    smallbutton 생성

--- client/src/Components/Header.Styled.js
+++ client/src/Components/Header.Styled.js
@@ -38,22 +38,3 @@ export const ButtonContainer = styled.div`
   gap: 10px;
   flex-shrink: 0;
 `;
-export const Button = styled.button`
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 74px;
-  height: 32px;
-  padding: 0;
-  border-radius: 19px;
-  background: #3f2305;
-  color: #fff;
-  font-family: Inter;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  cursor: pointer;
-  border: unset;
-`;
