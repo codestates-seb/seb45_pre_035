1a452e3b1256c84d613f453f1fd7c0600b0e774d
Moses-Yu <mosesyumss@gmail.com>
2023-08-17 11:03:32 +0900

    뒤로가기 버튼 배치 편하게 flex 가로

--- client/src/Pages/styles/PageStyle.js
+++ client/src/Pages/styles/PageStyle.js
@@ -2,6 +2,7 @@ import { styled } from 'styled-components';
 export const PageStyle = styled.div`
   padding: 65px 0px;
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+  column-gap: 20px;
 `;
