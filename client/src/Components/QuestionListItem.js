f2b4dcf182125e5ae39e31cd29f38ef5cf87e13f
seolmiseon <budaxige@gmail.com>
2023-08-17 16:45:58 +0900

    [#18] 질문props진행중

--- client/src/Components/QuestionListItem.js
+++ client/src/Components/QuestionListItem.js
@@ -1,9 +1,47 @@
-import React from "react";
-import { Styled } from './QuestionListItem';
+import React from 'react';
+import {
+  Question,
+  ContentCard,
+  BottomContainer,
+} from './QuestionListItem.Styled';
+import { useNavigate } from 'react-router-dom';
+import { AnswerIcon, LikeIcon } from './IconBtn';
 
-function QuestionList = () => {
-    const 
-    return (
-        
-    )
+export default function questionItem({ question }) {
+  const navigate = useNavigate();
+  const questiondetail = () => {
+    navigate('/questiondetail/:id');
+  };
+  const parsedDate = new Date(question.createdAt).toLocaleDateString('ko-kr');
+
+  const question = {
+    questionId,
+    title,
+    content,
+    createAt,
+    author,
+    answers: [].length,
+    likes: [].length,
+  };
+
+  return (
+    <Question onClick={questiondetail}>
+      <ContentCard className="question" id={question.id}>
+        <h2 className="title">{question.title}</h2>
+        <div className="question-message">{question.content}</div>
+        <BottomContainer>
+          <IconBtn>
+            <button src={AnswerIcon} onClick={question.answers} />
+
+            <button src={LikeIcon} onClick={question.likes} />
+          </IconBtn>
+          <div className="wrapper">
+            <span className="question-author">{question.author}</span>
+
+            <span className="question-createdAt">{parsedDate}</span>
+          </div>
+        </BottomContainer>
+      </ContentCard>
+    </Question>
+  );
 }
