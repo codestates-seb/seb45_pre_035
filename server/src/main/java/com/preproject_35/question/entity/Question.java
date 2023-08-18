41a934d483bb7606d85de0441df6578854520018
brightyein <wmfrjqsp@gmail.com>
2023-08-18 22:18:37 +0900

    database connect 성공

--- server/src/main/java/com/preproject_35/question/entity/Question.java
+++ server/src/main/java/com/preproject_35/question/entity/Question.java
@@ -1,5 +1,6 @@
 package com.preproject_35.question.entity;
 
+import com.preproject_35.answer.entity.Answer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -26,4 +29,13 @@ public class Question {
     private String content;
     private LocalDateTime createdAt = LocalDateTime.now();
     private boolean success = true;
+
+    // Question 에서만 Answer 정보 조회할거니까 1:N 단방향 엔티티 매핑
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Answer> answers = new ArrayList<>();
+
+    /**
+     * cascade = CascadeType.ALL : 부모와 자식 엔티티를 한 번에 영속화 + 한번에 제거
+     * fetch = FetchType.EAGER : 즉시로딩 , fetch = fetchType.LAZY : 지연로딩
+     */
 }
