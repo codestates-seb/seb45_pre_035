1e543b6f0bdfe3117965015ddc72c734fd7aad9b
seolmiseon <budaxige@gmail.com>
2023-08-20 18:01:04 +0900

    [#17] HOME페이지CSS 2일내내구현안되는중

---
+++ client/src/Pages/styles/HomeBox.js
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const HomeBox = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 27px;
+  width: 883px;
+  height: 681px;
+`;
