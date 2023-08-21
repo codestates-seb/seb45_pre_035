d2c44548e527f3bab7dc0c54f0130af30502bdd6
김제경 <kimjekyeong@gimjegyeong-ui-MacBookPro.local>
2023-08-22 02:59:53 +0900

    4

---
+++ server/src/main/java/com/preproject_35/answerComment/dto/AnswerCommentPostDto.java
@@ -0,0 +1,17 @@
+package com.preproject_35.answerComment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+public class AnswerCommentPostDto {
+    @Email
+    private String email;
+
+    @NotBlank
+    private String content;
+}
