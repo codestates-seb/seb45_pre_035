8f2ba6443c92787e1ad817b2cbeb27aa34161849
tjddnjs0102 <rjf654@gmail.com>
2023-08-21 23:56:33 +0900

    first cleaning code

--- server/src/main/java/com/preproject_35/element/answer/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.preproject_35.element.answer.repository;
-
-import com.preproject_35.element.answer.entity.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
