3476bc1812a35a0d363fdb2ccec988e73e0beab5
tjddnjs0102 <rjf654@gmail.com>
2023-08-22 10:30:56 +0900

    2

--- server/src/main/java/com/preproject_35/element/like/answer_like/AnswerLikeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.preproject_35.element.like.answer_like;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerLikeRepository extends JpaRepository<AnswerLike, Long> {
-    AnswerLike findByMemberIdAndAnswerId(long memberId, long answerId);
-}
